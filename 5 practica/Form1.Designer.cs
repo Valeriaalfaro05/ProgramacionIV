4d7764f3e3bbf611456afbcecf3dedc6c57e1760
Valeriaalfaro05 <paulavaleria.alfarocoronado@unidemat.edu.mx>
2025-02-20 00:17:10 -0600

    practica 5
    Dar de alta a personas

---
+++ 5 practica/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace _5_practica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnADD = new System.Windows.Forms.Button();
+            this.btnREMOVE = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(186, 70);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 26);
+            this.txtname.TabIndex = 0;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Location = new System.Drawing.Point(186, 138);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(100, 26);
+            this.txtLastname.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(595, 68);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(486, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "COUNTRY";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "NAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "LAST NAME";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(49, 230);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(703, 150);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "NAME";
+            this.Column1.MinimumWidth = 8;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "LAST NAME";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "COUNTRY";
+            this.Column3.MinimumWidth = 8;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // btnADD
+            // 
+            this.btnADD.Location = new System.Drawing.Point(85, 170);
+            this.btnADD.Name = "btnADD";
+            this.btnADD.Size = new System.Drawing.Size(95, 38);
+            this.btnADD.TabIndex = 7;
+            this.btnADD.Text = "ADD";
+            this.btnADD.UseVisualStyleBackColor = true;
+            this.btnADD.Click += new System.EventHandler(this.btnADD_Click);
+            // 
+            // btnREMOVE
+            // 
+            this.btnREMOVE.Location = new System.Drawing.Point(221, 170);
+            this.btnREMOVE.Name = "btnREMOVE";
+            this.btnREMOVE.Size = new System.Drawing.Size(108, 38);
+            this.btnREMOVE.TabIndex = 8;
+            this.btnREMOVE.Text = "REMOVE";
+            this.btnREMOVE.UseVisualStyleBackColor = true;
+            this.btnREMOVE.Click += new System.EventHandler(this.btnREMOVE_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnREMOVE);
+            this.Controls.Add(this.btnADD);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtLastname);
+            this.Controls.Add(this.txtname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button btnADD;
+        private System.Windows.Forms.Button btnREMOVE;
+    }
+}
+
---
+++ 5 practica/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace _5_practica
+{
+    public partial class Form1 : Form
+    {
+        int vindice;
+        string vseleccion;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("Mexico");
+            comboBox1.Items.Add("Canada");
+            comboBox1.Items.Add("USA");
+            comboBox1.Items.Add("Corea del sur");
+            comboBox1.Items.Add("China");
+            comboBox1.Items.Add("Colombia");
+        }
+
+        private void btnADD_Click(object sender, EventArgs e)
+        {
+            vseleccion = Convert.ToString(comboBox1.Text);
+            int vIndice = dataGridView1.Rows.Add();
+
+            dataGridView1.Rows[vIndice].Cells[0].Value = txtname.Text;
+            dataGridView1.Rows[vIndice].Cells[1].Value = txtLastname.Text;
+            dataGridView1.Rows[vIndice].Cells[2].Value = vseleccion;
+
+            txtname.Text = "";
+            txtLastname.Text = "";
+            comboBox1.Text = "";
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            vindice = e.RowIndex;
+        }
+
+        private void btnREMOVE_Click(object sender, EventArgs e)
+        {
+            if (vindice != -1)
+            {
+                dataGridView1.Rows.RemoveAt(vindice);
+            }
+
+            else
+            {
+                MessageBox.Show("Select a ITEM to delete");
+            }
+        }
+    }
+}

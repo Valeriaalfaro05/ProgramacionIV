da0cbca64107dae70a7b54533f2e23e889027d3a
Valeriaalfaro05 <paulavaleria.alfarocoronado@unidemat.edu.mx>
2025-02-21 09:38:20 -0600

    practica6
    promedio de tres parciales

---
+++ Practica6/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Practica6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtparcial1 = new System.Windows.Forms.TextBox();
+            this.txtparcial2 = new System.Windows.Forms.TextBox();
+            this.txtparcial3 = new System.Windows.Forms.TextBox();
+            this.promedio = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(184, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1er parcial";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(593, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2do parcial";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(582, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3er parcial";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtparcial1
+            // 
+            this.txtparcial1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtparcial1.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.txtparcial1.Location = new System.Drawing.Point(363, 94);
+            this.txtparcial1.Name = "txtparcial1";
+            this.txtparcial1.Size = new System.Drawing.Size(175, 26);
+            this.txtparcial1.TabIndex = 3;
+            // 
+            // txtparcial2
+            // 
+            this.txtparcial2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtparcial2.Location = new System.Drawing.Point(772, 152);
+            this.txtparcial2.Name = "txtparcial2";
+            this.txtparcial2.Size = new System.Drawing.Size(171, 26);
+            this.txtparcial2.TabIndex = 4;
+            // 
+            // txtparcial3
+            // 
+            this.txtparcial3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtparcial3.Location = new System.Drawing.Point(772, 94);
+            this.txtparcial3.Name = "txtparcial3";
+            this.txtparcial3.Size = new System.Drawing.Size(171, 26);
+            this.txtparcial3.TabIndex = 5;
+            // 
+            // promedio
+            // 
+            this.promedio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.promedio.Font = new System.Drawing.Font("MV Boli", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.promedio.Location = new System.Drawing.Point(191, 141);
+            this.promedio.Name = "promedio";
+            this.promedio.Size = new System.Drawing.Size(176, 69);
+            this.promedio.TabIndex = 6;
+            this.promedio.Text = "Promedio";
+            this.promedio.UseVisualStyleBackColor = false;
+            this.promedio.Click += new System.EventHandler(this.btnpromedio_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MV Boli", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(400, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 37);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Alumno";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.textBox4.Location = new System.Drawing.Point(535, 31);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(188, 26);
+            this.textBox4.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Location = new System.Drawing.Point(62, 239);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1234, 199);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "NOMBRE";
+            this.Column1.MinimumWidth = 8;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "1 PARCIAL";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "2 PARCIAL";
+            this.Column3.MinimumWidth = 8;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "3 PARCIAL";
+            this.Column4.MinimumWidth = 8;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 150;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "PROMEDIO";
+            this.Column5.MinimumWidth = 8;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 150;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "ESTATUS";
+            this.Column6.MinimumWidth = 8;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1343, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.promedio);
+            this.Controls.Add(this.txtparcial3);
+            this.Controls.Add(this.txtparcial2);
+            this.Controls.Add(this.txtparcial1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtparcial1;
+        private System.Windows.Forms.TextBox txtparcial2;
+        private System.Windows.Forms.TextBox txtparcial3;
+        private System.Windows.Forms.Button promedio;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
+
---
+++ Practica6/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnpromedio_Click(object sender, EventArgs e)
+        {
+            //OBTENER DATOS DE LOS TEXTBOX
+            string nombre = textBox4.Text;
+            double parcial1 = Convert.ToDouble(txtparcial1.Text);
+            double parcial2 = Convert.ToDouble(txtparcial2.Text);
+            double parcial3 = Convert.ToDouble(txtparcial3.Text);
+
+            //calculo el promedio de los tres parciales, con su respesctiva formula 
+            double promedio = (parcial1 + parcial2 + parcial3) / 3;
+
+            //determinar el estatus del alumno 
+            string Estatus;
+            if (promedio >= 95)
+            {
+                Estatus = "Exento";
+            }
+            else if (promedio >= 70)
+            {
+                Estatus = "Ordinario";
+            }
+            else if (promedio >= 50)
+            {
+                Estatus = "Extraordinario";
+            }
+            else
+            {
+                Estatus = "Especial";
+            }
+
+            int vIndice = dataGridView1.Rows.Add();
+            dataGridView1.Rows[vIndice].Cells[0].Value = textBox4.Text;
+            dataGridView1.Rows[vIndice].Cells[1].Value = txtparcial1.Text;
+            dataGridView1.Rows[vIndice].Cells[2].Value = txtparcial2.Text;
+            dataGridView1.Rows[vIndice].Cells[3].Value = txtparcial3.Text;
+            dataGridView1.Rows[vIndice].Cells[4].Value = promedio;
+            dataGridView1.Rows[vIndice].Cells[5].Value = Estatus;
+
+            textBox4.Text = "";
+            txtparcial1.Text = "";
+            txtparcial2.Text = "";
+            txtparcial3.Text = "";
+        }
+
+    }
+}
+
+    
+

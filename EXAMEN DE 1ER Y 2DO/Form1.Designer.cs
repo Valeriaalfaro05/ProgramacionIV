cff0e210e52baea0e2bf05bbbfb946fffc794617
Valeriaalfaro05 <paulavaleria.alfarocoronado@unidemat.edu.mx>
2025-02-26 14:13:40 -0600

    EXAMEN
    OBTENER MI SIGNO ZODIACAL CON LA FECHA DE NACIMIENTO

---
+++ EXAMEN DE 1ER Y 2DO/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace EXAMEN_DE_1ER_Y_2DO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(590, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MV Boli", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(77, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 96);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "CALCULAR SIGNO ZODIACAL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(544, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "INGRESA TU FECHA DE NACIMIENTO:";
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblresultado.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.Location = new System.Drawing.Point(378, 244);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(0, 41);
+            this.lblresultado.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(145, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(501, 41);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "OBTEN TU SIGNO ZODIACAL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(283, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 37);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "\"\"";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(743, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 55);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ EXAMEN DE 1ER Y 2DO/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EXAMEN_DE_1ER_Y_2DO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                // Obtener mi fecha de nacimiento en la caja de texto 
+                string input = textBox1.Text;
+
+
+                if (DateTime.TryParse(input, out DateTime fechaNacimiento))
+                {
+                    // Obtener mi signo zodiacal correcto con la fecha de nacimiento 
+                    string signo = ObtenerSignoZodiacal(fechaNacimiento.Day, fechaNacimiento.Month);
+                    label3.Text = $"Tu signo zodiacal es: {signo}";
+                }
+                else
+                {
+                    MessageBox.Show("La fecha ingresada no es válida. Usa el formato: dd/mm/yyyy.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            string ObtenerSignoZodiacal(int dia, int mes)
+            {
+                // Definir los rangos de fecg=has para cada signo zodiacal 
+                var signos = new[]
+                {
+            new { Signo = "Aries", MesInicio = 3, DiaInicio = 21, MesFin = 4, DiaFin = 19 },
+            new { Signo = "Tauro", MesInicio = 4, DiaInicio = 20, MesFin = 5, DiaFin = 20 },
+            new { Signo = "Géminis", MesInicio = 5, DiaInicio = 21, MesFin = 6, DiaFin = 20 },
+            new { Signo = "Cáncer", MesInicio = 6, DiaInicio = 21, MesFin = 7, DiaFin = 22 },
+            new { Signo = "Leo", MesInicio = 7, DiaInicio = 23, MesFin = 8, DiaFin = 22 },
+            new { Signo = "Virgo", MesInicio = 8, DiaInicio = 23, MesFin = 9, DiaFin = 22 },
+            new { Signo = "Libra", MesInicio = 9, DiaInicio = 23, MesFin = 10, DiaFin = 22 },
+            new { Signo = "Escorpio", MesInicio = 10, DiaInicio = 23, MesFin = 11, DiaFin = 21 },
+            new { Signo = "Sagitario", MesInicio = 11, DiaInicio = 22, MesFin = 12, DiaFin = 21 },
+            new { Signo = "Capricornio", MesInicio = 12, DiaInicio = 22, MesFin = 1, DiaFin = 19 },
+            new { Signo = "Acuario", MesInicio = 1, DiaInicio = 20, MesFin = 2, DiaFin = 18 },
+            new { Signo = "Piscis", MesInicio = 2, DiaInicio = 19, MesFin = 3, DiaFin = 20 }
+        };
+
+                // Buscar el signo zodiacal que corresponda 
+                foreach (var signoZodiacal in signos)
+                {
+                    if ((mes == signoZodiacal.MesInicio && dia >= signoZodiacal.DiaInicio) ||
+                        (mes == signoZodiacal.MesFin && dia <= signoZodiacal.DiaFin) ||
+                        (mes > signoZodiacal.MesInicio && mes < signoZodiacal.MesFin))
+                    {
+                        return signoZodiacal.Signo;
+                    }
+                }
+                //muestra un mensaje en caso de que ponga una fecha fuera de los rangos 
+                return "Fecha inválida"; 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+    }
+
